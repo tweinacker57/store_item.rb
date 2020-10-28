2138ff5c5b1a649f1fde5aae7b170babcd55dcf4
Tyler Weinacker <tweinacker57@gmail.com>
2020-10-27 21:07:47 -0400

    Hashes of cars

---
+++ store_item.rb
@@ -0,0 +1,8 @@
+car1 = {:make => "Ford", :model => "GT", :price => 120000}
+p "The #{car1[:make]} #{car1[:model]} is $#{car1[:price]}"
+
+car2 = {:make => "Aston Martin", :model => "Vanquish", :price => 250000}
+p "The #{car2[:make]} #{car2[:model]} is $#{car2[:price]}"
+
+car3 = {make: "Tesla", model: "Model S", price: 100000}
+p "The #{car3[:make]} #{car3[:model]} is $#{car3[:price]}"
